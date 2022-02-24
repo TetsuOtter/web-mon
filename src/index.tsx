530bff45ef22bee938618d8a8eaf3b68e9d2a24f
TetsuOtter <31824852+TetsuOtter@users.noreply.github.com>
2022-02-24 18:05:38 +0900

    add routing

--- src/index.tsx
+++ src/index.tsx
@@ -8,16 +8,47 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { reducer } from './redux/reducer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Lines } from './pages/Lines';
+import { Timetables } from './pages/Timetables';
+import { WestMON } from './pages/WestMON';
 
 const store = createStore(reducer);
+
+export const LINE_PAGE_URL = "/lines";
+export const TIMETABLE_SELECT_PAGE_URL = "/timetables";
+export const SHOW_TIMETABLE_PAGE_URL = "/show-timetable";
+
+export const WEST_MON_PAGE_ID = "westmon";
+
+export const LINE_ID_PARAM_NAME = "line_id";
+export const TIMETABLE_ID_PARAM_NAME = "timetable_id";
+export const STATION_ID_PARAM_NAME = "station_id";
 
 render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
         <Routes>
-          <Route path="/" element={<App/>}/>
-          <Route path="/abc" element={<App/>}/>
+          <Route
+            path="/"
+            element={<App />}
+          />
+          <Route
+            path={`${LINE_PAGE_URL}`}
+            element={<Lines />}
+          />
+          <Route
+            path={`${TIMETABLE_SELECT_PAGE_URL}/:${LINE_ID_PARAM_NAME}`}
+            element={<Timetables />}
+          />
+          <Route
+            path={`${SHOW_TIMETABLE_PAGE_URL}/:${LINE_ID_PARAM_NAME}/:${TIMETABLE_ID_PARAM_NAME}/${WEST_MON_PAGE_ID}/:${STATION_ID_PARAM_NAME}`}
+            element={<WestMON />}
+          />
         </Routes>
       </BrowserRouter>
     </Provider>
