c3ca3b4904421363791dc6638387fb1533d70ca1
TetsuOtter <31824852+TetsuOtter@users.noreply.github.com>
2022-03-04 12:12:57 +0900

    remove reportWebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 import { reducer } from './redux/reducer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Lines } from './pages/Lines';
@@ -60,8 +59,3 @@ render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
